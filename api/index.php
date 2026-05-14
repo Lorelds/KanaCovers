51577e1012b8e570632a3d579cc050aea9ff2583
Lorelds <69231971+Lorelds@users.noreply.github.com>
2026-05-14 14:03:02 +0700

    fix vercel storage

--- api/index.php
+++ api/index.php
@@ -1,5 +1,16 @@
 <?php
 /**
  * Forward Vercel requests to normal Laravel public/index.php
+ * and override storage paths to /tmp since Vercel is read-only
  */
+
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('SESSION_DRIVER=cookie');
+putenv('LOG_CHANNEL=stderr');
+
 require __DIR__ . '/../public/index.php';
